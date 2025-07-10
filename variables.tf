8400cc3fd1152581abde6b9431262d9de0e81b0d
everybodylovescode <fhalanxftw@gmail.com>
2025-07-10 17:21:56 +0000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "location" {
+  default = "westus2"
+}
+
+variable "resource_group" {
+  default = "student-blake-rg"
+}
+
+variable "vnet_name" {
+  default = "student-blake-vnet"
+}
+
+variable "subnet_name" {
+  default = "student-blake-subnet"
+}
+
+variable "vm_name" {
+  default = "student-blake-vm"
+}
+variable "subId" {
+default = "d52a6d4f-8a6b-4e23-a011-dbb0c8d7eddc"  
+}
