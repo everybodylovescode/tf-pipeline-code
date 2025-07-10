fedbdbf9d97c67f5d2b11ff92500f847b8476fa1
everybodylovescode <fhalanxftw@gmail.com>
2025-07-10 17:20:15 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,68 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "d52a6d4f-8a6b-4e23-a011-dbb0c8d7eddc"
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "blake-tf-ResourceGroup"
+  location = "westus2"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "myTFVNet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "main" {
+  name                 = "blake-tf-Subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "blake-tf-NIC"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "blake-tf-IPConfig"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "main" {
+  name                = "blake-tf-vm"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+  admin_password = "P@ssword1234!"
+  disable_password_authentication = false
+
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    name                 = "myosdisk"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+resource "azurerm_public_ip" "public_ip" {
+  name                = "${var.vm_name}-ip"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  allocation_method   = "Static"
+}
